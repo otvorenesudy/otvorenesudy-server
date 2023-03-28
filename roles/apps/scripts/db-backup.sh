ec742a2b49f035c674f27d916926c859162243c7
Samuel Molnar <molnar.samuel@gmail.com>
2023-03-28 22:03:30 +0200

    Downscale more

---
+++ roles/apps/scripts/db-backup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# set variables
+BACKUP_DIR=/root/db-backups
+TIMESTAMP=$(date +"%Y-%m-%d_%H-%M-%S")
+MAX_BACKUPS=5
+
+# create backup directory if it doesn't exist
+mkdir -p $BACKUP_DIR
+
+# run pg_dump and save to backup file with timestamp
+sudo -u postgres pg_dumpall > /tmp/db-backup-$TIMESTAMP.sql
+tar -czvf $BACKUP_DIR/db-backup-$TIMESTAMP.tar.gz /tmp/db-backup-$TIMESTAMP.sql 
+rm /tmp/db-backup-$TIMESTAMP.sql
+
+# remove old backups if there are more than MAX_BACKUPS
+cd $BACKUP_DIR
+BACKUP_FILES=($(ls -t db-backup-*.tar.gz))
+NUM_BACKUPS=${#BACKUP_FILES[@]}
+if [ $NUM_BACKUPS -gt $MAX_BACKUPS ]; then
+  REMOVE_BACKUPS=$((NUM_BACKUPS - MAX_BACKUPS))
+  for (( i=0; i<$REMOVE_BACKUPS; i++ )); do
+    rm ${BACKUP_FILES[$i]}
+  done
+fi
