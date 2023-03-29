d8e5d1ead250f111f5c5947a4d7aa4ea4794f26c
Samuel Molnar <molnar.samuel@gmail.com>
2023-03-29 16:13:53 +0200

    Optimise

--- roles/apps/scripts/self-recovery.sh
+++ roles/apps/scripts/self-recovery.sh
@@ -14,6 +14,8 @@ array=(
 
 for url in "${array[@]}"
 do
+    echo "self-recovery # Testing [$url] ..." | ts
+
     status=$(curl --max-time 15 -s -o /dev/null -w '%{http_code}\n' "${url}")
 
     if [[ $status -ne 200 ]]; then
@@ -23,16 +25,19 @@ do
       /bin/systemctl restart nginx
       /bin/systemctl restart elasticsearch
 
-      echo "self-recovery # Url [$url] returned [$status] status" | ts
+      echo "self-recovery # Url [$url] returned [$status] status and services restarted" | ts 1>&2
       exit 0
+    else 
+      echo "self-recovery # Url [$url] returned [$status] status" | ts
     fi
 done
 
 status=$(curl --max-time 15 -s -o /dev/null -w '%{http_code}\n' "http://localhost:9200/decrees_production/_search")
 
 if [[ $status -ne 200 ]]; then
-  echo "self-recovery # Elasticsearch timed out with [$status] status" | ts
+  echo "self-recovery # Elasticsearch timed out with [$status] status, restarting ..." | ts
   /bin/systemctl restart elasticsearch
+  echo "self-recovery # Elasticsearch restarted" | ts
 fi
 
 exit 0
