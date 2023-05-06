58569ea5f14ee04bde2037614ed7a84464c7da41
Samuel Molnar <molnar.samuel@gmail.com>
2023-05-06 21:01:22 +0200

    Fix sidekiq

--- roles/apps/scripts/sidekiq.sh
+++ roles/apps/scripts/sidekiq.sh
@@ -1,14 +1,14 @@
 source /home/deploy/.profile
 
-# cd /home/deploy/projects/otvorenesudy-production/current
-# /usr/bin/env kill -0 $(cat /home/deploy/projects/otvorenesudy-production/shared/tmp/pids/sidekiq.pid)
-# $HOME/.rbenv/bin/rbenv exec bundle exec sidekiqctl stop /home/deploy/projects/otvorenesudy-production/shared/tmp/pids/sidekiq.pid 10
-# $HOME/.rbenv/bin/rbenv exec bundle exec sidekiq --index 0 --pidfile /home/deploy/projects/otvorenesudy-production/shared/tmp/pids/# sidekiq.pid --environment production --logfile /home/deploy/projects/otvorenesudy-production/shared/log/sidekiq.log --daemon
+cd /home/deploy/projects/otvorenesudy-production/current
+/usr/bin/env kill -0 $(cat /home/deploy/projects/otvorenesudy-production/shared/tmp/pids/sidekiq.pid)
+$HOME/.rbenv/bin/rbenv exec bundle exec sidekiqctl stop /home/deploy/projects/otvorenesudy-production/shared/tmp/pids/sidekiq.pid 10
+$HOME/.rbenv/bin/rbenv exec bundle exec sidekiq --index 0 --pidfile /home/deploy/projects/otvorenesudy-production/shared/tmp/pids/# sidekiq.pid --environment production --logfile /home/deploy/projects/otvorenesudy-production/shared/log/sidekiq.log --daemon
 
-# cd /home/deploy/projects/otvorenesudy-api-production/current
-# $HOME/.rbenv/bin/rbenv exec bundle exec sidekiqctl stop /home/deploy/projects/otvorenesudy-api-production/shared/tmp/pids/sidekiq-0.pid 10
-# $HOME/.rbenv/bin/rbenv exec bundle exec sidekiq --index 0 --pidfile /home/deploy/projects/otvorenesudy-api-production/shared/tmp/pids/sidekiq-0.pid --environment production --logfile /home/deploy/projects/otvorenesudy-api-production/shared/log/sidekiq.log --daemon
+cd /home/deploy/projects/otvorenesudy-api-production/current
+$HOME/.rbenv/bin/rbenv exec bundle exec sidekiqctl stop /home/deploy/projects/otvorenesudy-api-production/shared/tmp/pids/sidekiq-0.pid 10
+$HOME/.rbenv/bin/rbenv exec bundle exec sidekiq --index 0 --pidfile /home/deploy/projects/otvorenesudy-api-production/shared/tmp/pids/sidekiq-0.pid --environment production --logfile /home/deploy/projects/otvorenesudy-api-production/shared/log/sidekiq.log --daemon
 
-# cd /home/deploy/projects/otvorenesudy-prokuratura-production/current
-# $HOME/.rbenv/bin/rbenv exec bundle exec sidekiqctl stop /home/deploy/projects/otvorenesudy-p,arokuratura-production/shared/tmp/pids/sidekiq-0.pid 10
-# $HOME/.rbenv/bin/rbenv exec bundle exec sidekiq --index 0 --pidfile /home/deploy/projects/otvorenesudy-prokuratura-production/shared/tmp/pids/sidekiq-0.pid --environment production --logfile /home/deploy/projects/otvorenesudy-prokuratura-production/shared/log/sidekiq.log --daemon
+cd /home/deploy/projects/otvorenesudy-prokuratura-production/current
+$HOME/.rbenv/bin/rbenv exec bundle exec sidekiqctl stop /home/deploy/projects/otvorenesudy-p,arokuratura-production/shared/tmp/pids/sidekiq-0.pid 10
+$HOME/.rbenv/bin/rbenv exec bundle exec sidekiq --index 0 --pidfile /home/deploy/projects/otvorenesudy-prokuratura-production/shared/tmp/pids/sidekiq-0.pid --environment production --logfile /home/deploy/projects/otvorenesudy-prokuratura-production/shared/log/sidekiq.log --daemon
